10759e6abff3f5b60ef665f277a35636c36c7d79
ragimovalik <ragimovalik@gmail.com>
2021-06-22 10:31:57 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// import Form from './components/Forms';
-import Buttons from './components/Buttons';
+import Forms from './components/Forms';
+// import Buttons from './components/Buttons';
 
 function App() {
   return (
     <div>
-      <h1 style={{ color: 'purple', margin: 10 }}> New App File</h1>
+      <h1 style={{ color: '#252b31', margin: 10 }}> New App File</h1>
 
-      <Buttons buttonSize={'large'} />
-      <Buttons />
-      <Buttons buttonSize={'small'} />
+      <Forms />
+      {/* <Buttons buttonSize={'large'} /> */}
+      {/* <Buttons /> */}
+      {/* <Buttons buttonSize={'small'} /> */}
     </div>
   );
 }
